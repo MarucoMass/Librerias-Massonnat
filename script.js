3c12c8dc52442605933d3d9189509152bdfe01fa
Mario <mariomass23@gmail.com>
2022-03-13 23:31:12 -0300

    cambios del carrito

--- script.js
+++ script.js
@@ -17,38 +17,126 @@ const cursosCello = new Curso(9, 'Curso de Cello', 8500)
 const cursosSaxo = new Curso(10, 'Curso de Saxo', 9000)
 let arrayCursos = []
 let arrayCompra = []
-let newArray = []
+// let newArray = []
+let contador = document.getElementById('carritoContador')
+let contadorDos = document.getElementById('carritoContadorDos')
+let cuenta = 0
+const carritoBtn = document.querySelectorAll('.carritoBtn')
 const cursos_boxes = document.getElementById('cursos_boxes')
+let boxCarrito = document.querySelector('.boxCarrito')
+const boxLista = document.getElementById('boxLista')
 // let boxes = document.querySelectorAll('.cursos_box')
 
 if (localStorage.getItem('cursos')) {
     arrayCompra = JSON.parse(localStorage.getItem('cursos'))
+    arrayCompra.forEach((value) => {
+        boxLista.innerHTML += `
+        <div class="boxCompra">
+            <h4>${value.nombre}</h4>
+            <h5>Precio: $ ${value.precio}</h5>
+            <button value="${value.id}" class="btnBorrar">Borrar</button>
+        </div>
+        <hr>
+        `
+    })
 } else {
     localStorage.setItem('cursos', JSON.stringify(arrayCompra))
 }
 arrayCursos.push(cursosGtrElec, cursosGtrEsp, cursosPiano, cursosBajo, cursosCanto, cursosTrompeta, cursosFlauta, cursosViolin, cursosCello, cursosSaxo)
-arrayCursos.forEach((value) => {
-    cursos_boxes.innerHTML += `
-    <div class="cursos_box" id="curso${value.id}">
-    <h3>${value.nombre}</h3>
-    <p>Este curso tiene un costo de $${value.precio}.</p>
-    <button value="${value.id}">Agregar al carrito</button>
-</div>   `
-})
+
+const cargarCursos = () => {
+    arrayCursos.forEach((value) => {
+        cursos_boxes.innerHTML += `
+        <div class="cursos_box" id="curso${value.id}">
+        <h3>${value.nombre}</h3>
+        <p>Este curso tiene un costo de $${value.precio}.</p>
+        <button value="${value.id}">Agregar al carrito</button>
+    </div>   `
+    })
+}
 
 // localStorage.clear()
-cursos_boxes.addEventListener('click', (e) => {
-    if(e.target.value){
-        // newArray = arrayCursos.filter((el) => el.id == e.target.value)
-        let finder = arrayCursos.find((el) => el.id == e.target.value)
-        let id = finder.id
-        if (!arrayCompra.some((el) => el.id == id)) {
-            arrayCompra.push(finder)
-            localStorage.setItem('cursos', JSON.stringify(arrayCompra))
-        } 
-    }
-})
+const comprarCursos = () =>{
+    cursos_boxes.addEventListener('click', (e) => {
+        if(e.target.value){
+            let finder = arrayCursos.find((el) => el.id == e.target.value)
+            let id = finder.id
+            if (!arrayCompra.some((el) => el.id == id)) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Se ha añadido al carrito',
+                    text: 'Gracias!',
+                })
+                arrayCompra.push(finder)
+                localStorage.setItem('cursos', JSON.stringify(arrayCompra))
+                mostrarCursos()
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Ya elegiste ese curso',
+                    text: 'Elige alguno de los otros!',
+                })
+            }
+        }
+    })
+}
 
+carritoBtn.forEach(el => el.addEventListener('click', () => {
+    if (boxLista.innerHTML == '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'No hay cursos',
+            text: 'Elige alguno de los cursos!',
+        })
+        boxCarrito.classList.remove('active')
+    } else {
+        boxCarrito.classList.toggle('active')
+    }
+}))
+
+const mostrarCursos = () => {
+    boxLista.innerHTML = ''
+    let cursoComprado = JSON.parse(localStorage.getItem('cursos'))
+    cursoComprado.forEach((value) => {
+        boxLista.innerHTML += `
+        <div class="boxCompra">
+            <h4>${value.nombre}</h4>
+            <h5>Precio: $ ${value.precio}</h5>
+            <button value="${value.id}" class="btnBorrar">Borrar</button>
+        </div>
+        <hr>
+        `
+    })
+}
+const borrarCursos = () => {
+    boxLista.addEventListener('click', (e) => {
+    if (boxLista.innerHTML != '') {
+        boxLista.innerHTML = ''
+        let cursoComprado = JSON.parse(localStorage.getItem('cursos'))
+        arrayCompra = cursoComprado.filter(el => el.id != e.target.value)
+        localStorage.removeItem('cursos')
+        localStorage.setItem('cursos', JSON.stringify(arrayCompra))
+        arrayCompra.forEach((value) => {
+            boxLista.innerHTML += `
+            <div class="boxCompra">
+                <h4>${value.nombre}</h4>
+                <h5>Precio: $ ${value.precio}</h5>
+                <button value="${value.id}" class="btnBorrar">Borrar</button>
+            </div>
+            <hr>
+            `
+        })
+        if(boxLista.innerHTML == '') {
+            boxCarrito.classList.remove('active')
+        }
+    } 
+})
+}
+
+
+cargarCursos()
+comprarCursos()
+borrarCursos()
 
 
 // ****************************************************************************************
